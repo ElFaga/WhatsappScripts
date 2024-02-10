37310f74b2b064a0a73074785fea0f8400adcf87
EhOFagas <65435729+ElFaga@users.noreply.github.com>
2024-02-09 20:22:40 -0400

    Update CopaPistao-PT-BR.js

--- CopaPistao-PT-BR.js
+++ CopaPistao-PT-BR.js
@@ -1,4 +1,29 @@
-🚨 Como é a estrutura da Copa Pistão?
+async function enviarScript(scriptText){
+
+    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+    main = document.querySelector("#main"),
+    textarea = main.querySelector(`div[contenteditable="true"]`)
+    
+    if(!textarea) throw new Error("Não há uma conversa aberta")
+    
+    for(const line of lines){
+        console.log(line)
+    
+        textarea.focus();
+        document.execCommand('insertText', false, line);
+        textarea.dispatchEvent(new Event('change', {bubbles: true}));
+    
+        setTimeout(() => {
+            (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+        }, 100);
+        
+        if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+    }
+    
+    return lines.length;
+}
+
+enviarScript(`🚨 Como é a estrutura da Copa Pistão?
 
 ➤ 1940 - 1950:
 Temporada regular = 20 corridas
