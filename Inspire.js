c0d7b2dd804c0a4c7fdce19e47bc58ca5f53f567
EhOFagas <65435729+ElFaga@users.noreply.github.com>
2024-02-09 20:28:19 -0400

    Create Inspire.js

---
+++ Inspire.js
@@ -0,0 +1,173 @@
+Inspire.
+
+Inspire o máximo de ar que conseguir. Essa estória deve durar aproximadamente o tempo que você consegue segurar sua respiração, e um pouco mais. Então escute o mais rápido que puder.
+
+Um amigo meu aos 13 anos ouviu falar sobre “fio-terra”. Isso é quando alguém enfia um consolo na bunda. Estimule a próstata o suficiente, e os rumores dizem que você pode ter orgasmos explosivos sem usar as mãos. Nessa idade, esse amigo é um pequeno maníaco sexual. Ele está sempre buscando uma melhor forma de gozar. Ele sai para comprar uma cenoura e lubrificante. Para conduzir uma pesquisa particular. Ele então imagina como seria a cena no caixa do supermercado, a solitária cenoura e o lubrificante percorrendo pela esteira o caminho até o atendente no caixa. Todos os clientes esperando na fila, observando. Todos vendo a grande noite que ele preparou.
+
+Então, esse amigo compra leite, ovos, açúcar e uma cenoura, todos os ingredientes para um bolo de cenoura. E vaselina.
+
+Como se ele fosse para casa enfiar um bolo de cenoura no rabo.
+
+Em casa, ele corta a ponta da cenoura com um alicate. Ele a lubrifica e desce seu traseiro por ela. Então, nada. Nenhum orgasmo. Nada acontece, exceto pela dor.
+
+Então, esse garoto, a mãe dele grita dizendo que é a hora da janta. Ela diz para descer, naquele momento.
+
+Ele remove a cenoura e coloca a coisa pegajosa e imunda no meio das roupas sujas debaixo da cama.
+
+Depois do jantar, ele procura pela cenoura, e não está mais lá. Todas as suas roupas sujas, enquanto ele jantava, foram recolhidas por sua mãe para lavá-las. Não havia como ela não encontrar a cenoura, cuidadosamente esculpida com uma faca da cozinha, ainda lustrosa de lubrificante e fedorenta.
+
+Esse amigo meu, ele espera por meses na surdina, esperando que seus pais o confrontem. E eles nunca fazem isso. Nunca. Mesmo agora que ele cresceu, aquela cenoura invisível aparece em toda ceia de Natal, em toda festa de aniversário. Em toda caça de ovos de páscoa com seus filhos, os netos de seus pais, aquela cenoura fantasma paira por sobre todos eles. Isso é algo vergonhoso demais para dar um nome.
+
+As pessoas na França possuem uma expressão: “sagacidade de escadas.” Em francês: esprit de l’escalier. Representa aquele momento em que você encontra a resposta, mas é tarde demais. Digamos que você está numa festa e alguém o insulta. Você precisa dizer algo. Então sob pressão, com todos olhando, você diz algo estúpido. Mas no momento em que sai da festa….
+
+Enquanto você desce as escadas, então – mágica. Você pensa na coisa mais perfeita que poderia ter dito. A réplica mais avassaladora.
+
+Esse é o espírito da escada.
+
+O problema é que até mesmo os franceses não possuem uma expressão para as coisas estúpidas que você diz sob pressão. Essas coisas estúpidas e desesperadas que você pensa ou faz.
+
+Alguns atos são baixos demais para receberem um nome. Baixos demais para serem discutidos.
+
+Agora que me recordo, os especialistas em psicologia dos jovens, os conselheiros escolares, dizem que a maioria dos casos de suicídio adolescente eram garotos se estrangulando enquanto se masturbavam. Seus pais o encontravam, uma toalha enrolada em volta do pescoço, a toalha amarrada no suporte de cabides do armário, o garoto morto. Esperma por toda a parte. É claro que os pais limpavam tudo. Colocavam calças no garoto. Faziam parecer… melhor. Ao menos, intencional. Um caso comum de triste suicídio adolescente.
+
+Outro amigo meu, um garoto da escola, seu irmão mais velho na Marinha dizia como os caras do Oriente Médio se masturbavam de forma diferente do que fazemos por aqui. Esse irmão tinha desembarcado num desses países cheios de camelos, na qual o mercado público vendia o que pareciam abridores de carta chiques. Cada uma dessas coisas é apenas um fino cabo de latão ou prata polida, do comprimento aproximado de sua mão, com uma grande ponta numa das extremidades, ou uma esfera de metal ou uma dessas empunhaduras como as de espadas. Esse irmão da Marinha dizia que os árabes ficavam de pau duro e inseriam esse cabo de metal dentro e por toda a extremidade de seus paus. Eles então batiam punheta com o cabo dentro, e isso os faziam gozar melhor. De forma mais intensa.
+
+Esse irmão mais velho viajava pelo mundo, mandando frases em francês. Frases em russo. Dicas de punhetagem.
+
+Depois disso, o irmão mais novo, um dia ele não aparece na escola. Naquela noite, ele liga pedindo para eu pegar seus deveres de casa pelas próximas semanas. Porque ele está no hospital.
+
+Ele tem que compartilhar um quarto com velhos que estiveram operando as entranhas. Ele diz que todos compartilham a mesma televisão. Que a única coisa para dar privacidade é uma cortina. Seus pais não o vem visitar. No telefone, ele diz como os pais dele queriam matar o irmão mais velho da Marinha.
+
+Pelo telefone, o garoto diz que, no dia anterior, ele estava meio chapado. Em casa, no seu quarto, ele deitou-se na cama. Ele estava acendendo uma vela e folheando algumas revistas pornográficas antigas, preparando-se para bater uma. Isso foi depois que ele recebeu as notícias de seu irmão marinheiro. Aquela dica de como os árabes se masturbam. O garoto olha ao redor procurando por algo que possa servir. Uma caneta é grande demais. Um lápis, grande demais e áspero. Mas escorrendo pelo canto da vela havia um fino filete de vela derretida que poderia servir. Com as pontas dos dedos, o garoto descola o filete da vela. Ele o enrola na palma de suas mãos. Longo, e liso, e fino.
+
+Chapado e com tesão, ele enfia lá dentro, mais e mais fundo por dentro do canal urinário de seu pau. Com uma boa parte da cera ainda para fora, ele começa o trabalho.
+
+Até mesmo nesse momento ele reconhece que esses árabes eram caras muito espertos. Eles reinventaram totalmente a punheta. Deitado totalmente na cama, as coisas estão ficando tão boas que o garoto nem observa a filete de cera. Ele está quase gozando quando percebe que a cera não está mais lá.
+
+O fino filete de cera entrou. Bem lá no fundo. Tão fundo que ele nem consegue sentir a cera dentro de seu pau.
+
+Das escadas, sua mãe grita dizendo que é a hora da janta. Ela diz para ele descer naquele momento. O garoto da cenoura e o garoto da cera eram pessoas diferentes, mas viviam basicamente a mesma vida.
+
+Depois do jantar, as entranhas do garoto começam a doer. É cera, então ele imagina que ela vá derreter dentro dele e ele poderá mijar para fora. Agora suas costas doem. Seus rins. Ele não consegue ficar ereto corretamente.
+
+O garoto falando pelo telefone do seu quarto de hospital, no fundo pode-se ouvir campainhas, pessoas gritando. Game shows.
+
+Os raios-X mostram a verdade, algo longo e fino, dobrado dentro de sua bexiga. Esse longo e fino V dentro dele está coletando todos os minerais no seu mijo. Está ficando maior e mais expesso, coletando cristais de cálcio, está batendo lá dentro, rasgando a frágil parede interna de sua bexiga, bloqueando a urina. Seus rins estão cheios. O pouco que sai de seu pau é vermelho de sangue.
+
+O garoto e seus pais, a família inteira, olhando aquela chapa de raio-X com o médico e as enfermeiras ali, um grande V de cera brilhando na chapa para todos verem, ele deve falar a verdade. Sobre o jeito que os árabes se masturbam. Sobre o que o seu irmãos mais velho da Marinha escreveu.
+
+No telefone, nesse momento, ele começa a chorar.
+
+Eles pagam pela operação na bexiga com o dinheiro da poupança para sua faculdade. Um erro estúpido, e agora ele nunca mais será um advogado.
+
+Enfiando coisas dentro de você. Enfiando-se dentro de coisas. Uma vela no seu pau ou seu pescoço num nó, sabíamos que não poderia acabar em problemas.
+
+O que me fez ter problemas, eu chamava de Pesca Submarina. Isso era bater punheta embaixo d’água, sentando no fundo da piscina dos meus pais. Pegando fôlego, eu afundava até o fundo da piscina e tirava meu calção. Eu sentava no fundo por dois, três, quatro minutos.
+
+Só de bater punheta eu tinha conseguido uma enorme capacidade pulmonar. Se eu tivesse a casa só para mim, eu faria isso a tarde toda. Depois que eu gozava, meu esperma ficava boiando em grandes e gordas gotas.
+
+Depois disso eram mais alguns mergulhos, para apanhar todas. Para pegar todas e colocá-las em uma toalha. Por isso chamava de Pesca Submarina. Mesmo com o cloro, havia a minha irmã para se preocupar. Ou, Cristo, minha mãe.
+
+Esse era meu maior medo: minha irmã adolescente e virgem, pensando que estava ficando gorda e dando a luz a um bebê retardado de duas cabeças. As duas parecendo-se comigo. Eu, o pai e o tio. No fim, são as coisas nais quais você não se preocupa que te pegam.
+
+A melhor parte da Pesca Submarina era o duto da bomba do filtro. A melhor parte era ficar pelado e sentar nela.
+
+Como os franceses dizem, Quem não gosta de ter seu cú chupado? Mesmo assim, num minuto você é só um garoto batendo uma, e no outro nunca mais será um advogado.
+
+Num minuto eu estou no fundo da piscina e o céu é um azul claro e ondulado, aparecendo através de dois metros e meio de água sobre minha cabeça. Silêncio total exceto pelas batidas do coração que escuto em meu ouvido. Meu calção amarelo-listrado preso em volta do meu pescoço por segurança, só em caso de algum amigo, um vizinho, alguém que apareça e pergunte porque faltei aos treinos de futebol. O constante chupar da saída de água me envolve enquanto delicio minha bunda magra e branquela naquela sensação.
+
+Num momento eu tenho ar o suficiente e meu pau está na minha mão. Meus pais estão no trabalho e minha irmão no balé. Ninguém estará em casa por horas.
+
+Minhas mãos começam a punhetar, e eu paro. Eu subo para pegar mais ar. Afundo e sento no fundo.
+
+Faço isso de novo, e de novo.
+
+Deve ser por isso que garotas querem sentar na sua cara. A sucção é como dar uma cagada que nunca acaba. Meu pau duro e meu cú sendo chupado, eu não preciso de mais ar. O bater do meu coração nos ouvidos, eu fico no fundo até as brilhantes estrelas de luz começarem a surgir nos meus olhos. Minhas pernas esticadas, a batata das pernas esfregando-se contra o fundo. Meus dedos do pé ficando azul, meus dedos ficando enrugados por estar tanto tempo na água.
+
+E então acontece. As gotas gordas de gozo aparecem. É nesse momento que preciso de mais ar. Mas quando tento sair do fundo, não consigo. Não consigo colocar meus pés abaixo de mim. Minha bunda está presa.
+
+Médicos de plantão de emergência podem confirmar que todo ano cerca de 150 pessoas ficam presas dessa forma, sugadas pelo duto do filtro de piscina. Fique com o cabelo preso, ou o traseiro, e você vai se afogar. Todo o ano, muita gente fica. A maioria na Flórida.
+
+As pessoas simplesmente não falam sobre isso. Nem mesmo os franceses falam sobre tudo. Colocando um joelho no fundo, colocando um pé abaixo de mim, eu empurro contra o fundo. Estou saindo, não mais sentado no fundo da piscina, mas não estou chegando para fora da água também.
+
+Ainda nadando, mexendo meus dois braços, eu devo estar na metade do caminho para a superfície mas não estou indo mais longe que isso. O bater do meu coração no meu ouvido fica mais alto e mais forte.
+
+As brilhantes fagulhas de luz passam pelos meus olhos, e eu olho para trás… mas não faz sentido. Uma corda espessa, algum tipo de cobra, branco-azulada e cheia de veias, saiu do duto da piscina e está segurando minha bunda. Algumas das veias estão sangrando, sangue vermelho que aparenta ser preto debaixo da água, que sai por pequenos cortes na pálida pele da cobra. O sangue começa a sumir na água, e dentro da pele fina e branco-azulada da cobra é possível ver pedaços de alguma refeição semi-digerida.
+
+Só há uma explicação. Algum horrível monstro marinho, uma serpente do mar, algo que nunca viu a luz do dia, estava se escondendo no fundo escuro do duto da piscina, só esperando para me comer.
+
+Então… eu chuto a coisa, chuto a pele enrugada e escorregadia cheia de veias, e parece que mais está saindo do duto. Deve ser do tamanho da minha perna nesse momento, mas ainda segurando firme no meu cú. Com outro chute, estou a centímetros de conseguir respirar. Ainda sentido a cobra presa no meu traseiro, estou bem próximo de escapar.
+
+Dentro da cobra, é possível ver milho e amendoins. E dá pra ver uma brilhante esfera laranja. É um daqueles tipos de vitamina que meu pai me força a tomar, para poder ganhar massa. Para conseguir a bolsa como jogador de futebol. Com ferro e ácidos graxos Ômega 3.
+
+Ver essa pílula foi o que me salvou a vida.
+
+Não é uma cobra. É meu intestino grosso e meu cólon sendo puxados para fora de mim. O que os médicos chamam de prolapso de reto. São minhas entranhas sendo sugadas pelo duto.
+
+Os médicos de plantão de emergência podem confirmar que uma bomba de piscina pode puxar 300 litros de água por minuto. Isso corresponde a 180 quilos de pressão. O grande problema é que somos todos interconectados por dentro. Seu traseiro é apenas o término da sua boca. Se eu deixasse, a bomba continuaria a puxar minhas entranhas até que chegasse na minha língua. Imagine dar uma cagada de 180 quilos e você vai perceber como isso pode acontecer.
+
+O que eu posso dizer é que suas entranhas não sentem tanta dor. Não da forma que sua pele sente dor. As coisas que você digere, os médicos chamam de matéria fecal. No meio disso tudo está o suco gástrico, com pedaços de milho, amendoins e ervilhas.
+
+Essa sopa de sangue, milho, merda, esperma e amendoim flutua ao meu redor. Mesmo com minhas entranhas saindo pelo meu traseiro, eu tentando segurar o que restou, mesmo assim, minha vontade é de colocar meu calção de alguma forma.
+
+Deus proíba que meus pais vejam meu pau.
+
+Com uma mão seguro a saída do meu rabo, com a outra mão puxo o calção amarelo-listrado do meu pescoço. Mesmo assim, é impossível puxar de volta.
+
+Se você quer sentir como seria tocar seus intestinos, compre um camisinha feita com intestino de carneiro. Pegue uma e desenrole. Encha de manteiga de amendoim. Lubrifique e coloque debaixo d’água. Então tente rasgá-la. Tente partir em duas. É firme e ao mesmo tempo macia. É tão escorregadia que não dá para segurar.
+
+Uma camisinha dessas é feita do bom e velho intestino.
+
+Você então vê contra o que eu lutava.
+
+Se eu largo, sai tudo.
+
+Se eu nado para a superfície, sai tudo.
+
+Se eu não nadar, me afogo.
+
+É escolher entre morrer agora, e morrer em um minuto.
+
+O que meus pais vão encontrar depois do trabalho é um feto grande e pelado, todo curvado. Mergulhado na árgua turva da piscina de casa. Preso ao fundo por uma larga corda de veias e entranhas retorcidas. O oposto do garoto que se estrangula enquanto bate uma. Esse é o bebê que trouxeram para casa do hospital há 13 anos. Esse é o garoto que esperavam conseguir uma bolsa de jogador de futebol e eventualmente um mestrado. Que cuidaria deles quando estivessem velhinhos. Seus sonhos e esperanças. Flutuando aqui, pelado e morto. Em volta dele, gotas gordas de esperma.
+
+Ou isso, ou meus pais me encontrariam enrolado numa toalha encharcada de sangue, morto entre a piscina e o telefone da cozinha, os restos destroçados das minhas entranhas para fora do meu calção amarelo-listrado.
+
+Algo sobre o qual nem os franceses falam.
+
+Aquele irmão mais velho na Marinha, ele ensinou uma outra expressão bacana. Uma expressão russa. Do jeito que nós falamos “Preciso disso como preciso de um buraco na cabeça…,” os russos dizem, “Preciso disso como preciso de dentes no meu cú……
+
+Mne eto nado kak zuby v zadnitse.
+
+Essas histórias de como animais presos em armadilhas roem a própria perna fora, bem, qualquer coiote poderá te confirmar que algumas mordidas são melhores que morrer.
+
+Droga… mesmo se você for russo, um dia vai querer esses dentes.
+
+Senão, o que você pode fazer é se curvar todo. Você coloca um cotovelo por baixo do joelho e puxa essa perna para o seu rosto. Você morde e rói seu próprio cú. Se você ficar sem ar você consegue roer qualquer coisa para poder respirar de novo.
+
+Não é algo que seja bom contar a uma garota no primeiro encontro. Não se você espera por um beijinho de despedida. Se eu contasse como é o gosto, vocês não comeriam mais frutos do mar.
+
+É difícil dizer o que enojaria mais meus pais: como entrei nessa situação, ou como me salvei. Depois do hospital, minha mãe dizia, “Você não sabia o que estava fazendo, querido. Você estava em choque.” E ela teve que aprender a cozinhar ovos pochê.
+
+Todas aquelas pessoas enojadas ou sentindo pena de mim….
+
+Precisava disso como precisaria de dentes no cú.
+
+Hoje em dia, as pessoas sempre me dizem que eu sou magrinho demais. As pessoas em jantares ficam quietas ou bravas quando não como o cozido que fizeram. Cozidos podem me matar. Presuntadas. Qualquer coisa que fique mais que algumas horas dentro de mim, sai ainda como comida. Feijões caseiros ou atum, eu levanto e encontro aquilo intacto na privada.
+
+Depois que você passa por uma lavagem estomacal super-radical como essa, você não digere carne tão bem. A maioria das pessoas tem um metro e meio de intestino grosso. Eu tenho sorte de ainda ter meus quinze centímetros. Então nunca consegui minha bolsa de jogador de futebol. Nunca consegui meu mestrado. Meus dois amigos, o da cera e o da cenoura, eles cresceram, ficaram grandes, mas eu nunca pesei mais do que pesava aos 13 anos.
+
+Outro problema foi que meus pais pagaram muita grana naquela piscina. No fim meu pai teve que falar para o cara da limpeza da piscina que era um cachorro. O cachorro da família caiu e se afogou. O corpo sugado pelo duto. Mesmo depois que o cara da limpeza abriu o filtro e removeu um tubo pegajoso, um pedaço molhado de intestino com uma grande vitamina laranja dentro, mesmo assim meu pai dizia, “Aquela porra daquele cachorro era maluco.”
+
+Mesmo do meu quarto no segundo andar, podia ouvir meu pai falar, “Não dava para deixar aquele cachorro sozinho por um segundo….”
+
+E então a menstruação da minha irmã atrasou.
+
+Mesmo depois que trocaram a água da piscina, depois que vendemos a casa e mudamos para outro estado, depois do aborto da minha irmã, mesmo depois de tudo isso meus pais nunca mencionaram mais isso novamente.
+
+Nunca.
+
+Essa é a nossa cenoura invisível.
+
+Você. Agora você pode respirar.
+
+Eu ainda não
