be25d238f031eaf90c940690489def6b38d20514
EhOFagas <65435729+ElFaga@users.noreply.github.com>
2024-02-09 20:29:02 -0400

    Update Inspire.js

--- Inspire.js
+++ Inspire.js
@@ -1,3 +1,30 @@
+async function enviarScript(scriptText){
+
+    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+    main = document.querySelector("#main"),
+    textarea = main.querySelector(`div[contenteditable="true"]`)
+    
+    if(!textarea) throw new Error("Não há uma conversa aberta")
+    
+    for(const line of lines){
+        console.log(line)
+    
+        textarea.focus();
+        document.execCommand('insertText', false, line);
+        textarea.dispatchEvent(new Event('change', {bubbles: true}));
+    
+        setTimeout(() => {
+            (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+        }, 100);
+        
+        if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+    }
+    
+    return lines.length;
+}
+
+enviarScript(`
+
 Inspire.
 
 Inspire o máximo de ar que conseguir. Essa estória deve durar aproximadamente o tempo que você consegue segurar sua respiração, e um pouco mais. Então escute o mais rápido que puder.
@@ -171,3 +198,4 @@ Essa é a nossa cenoura invisível.
 Você. Agora você pode respirar.
 
 Eu ainda não
+`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
